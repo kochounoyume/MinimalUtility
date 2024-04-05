fffb1af1841b30a64af87535437b726e1ed345de
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-04-05 09:54:35 +0900

    Add: 簡単なFPSとメモリ表示をする実装を追加

---
+++ Packages/MinimalUtility/Runtime/R3/ObservableExtensions.Trigger.cs
@@ -0,0 +1,19 @@
+﻿#if ENABLE_R3
+using R3;
+using UnityEngine;
+
+namespace MinimalUtility.R3
+{
+    public static partial class ObservableExtensions
+    {
+        public static Observable<Unit> OnGUIAsObservable(this GameObject gameObject)
+        {
+            if (gameObject == null) return Observable.Empty<Unit>();
+            return
+                gameObject.TryGetComponent(out ObservableOnGUITrigger trigger)
+                    ? trigger.OnGUIAsObservable()
+                    : gameObject.AddComponent<ObservableOnGUITrigger>().OnGUIAsObservable();
+        }
+    }
+}
+#endif
---
+++ Packages/MinimalUtility/Runtime/R3/ObservableOnGUITrigger.cs
@@ -0,0 +1,32 @@
+﻿using R3;
+using R3.Triggers;
+using UnityEngine;
+
+namespace MinimalUtility.R3
+{
+    [DisallowMultipleComponent]
+    public sealed class ObservableOnGUITrigger : ObservableTriggerBase
+    {
+        Subject<Unit> onGUI;
+
+        /// <summary>OnGUI is called for rendering and handling GUI events.</summary>
+        void OnGUI()
+        {
+            if (onGUI != null) onGUI?.OnNext(Unit.Default);
+        }
+
+        /// <summary>OnGUI is called for rendering and handling GUI events.</summary>
+        public Observable<Unit> OnGUIAsObservable()
+        {
+            return onGUI ??= new Subject<Unit>();
+        }
+
+        protected override void RaiseOnCompletedOnDestroy()
+        {
+            if (onGUI != null)
+            {
+                onGUI.OnCompleted();
+            }
+        }
+    }
+}
