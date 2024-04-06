45b678a0e37a2c2946580cb52ee1130881beccd7
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-04-07 00:50:28 +0900

    Update Debug operator

--- Packages/MinimalUtility/Runtime/R3/ObservableExtensions.Debug.cs
+++ Packages/MinimalUtility/Runtime/R3/ObservableExtensions.Debug.cs
@@ -1,4 +1,5 @@
 ﻿#if ENABLE_R3
+using System;
 using R3;
 
 namespace MinimalUtility.R3
@@ -22,11 +23,35 @@ namespace MinimalUtility.R3
             return source
                 .Do(
                     state: l,
-                    onNext: (x, lb) => UnityEngine.Debug.Log($"{lb}OnNext({x})"),
-                    onErrorResume: (x, lb) => UnityEngine.Debug.Log($"{lb}OnErrorResume({x})"),
-                    onCompleted: (x, lb) => UnityEngine.Debug.Log($"{lb}OnCompleted({x})"),
-                    onSubscribe: lb => UnityEngine.Debug.Log($"{lb}OnSubscribe"),
-                    onDispose: lb => UnityEngine.Debug.Log($"{lb}OnDispose"));
+                    onNext: static (x, lb) => UnityEngine.Debug.Log($"{lb}OnNext({x})"),
+                    onErrorResume: static (x, lb) => UnityEngine.Debug.Log($"{lb}OnErrorResume({x})"),
+                    onCompleted: static (x, lb) => UnityEngine.Debug.Log($"{lb}OnCompleted({x})"),
+                    onSubscribe: static lb => UnityEngine.Debug.Log($"{lb}OnSubscribe"),
+                    onDispose: static lb => UnityEngine.Debug.Log($"{lb}OnDispose"));
+#else
+            return source;
+#endif
+        }
+
+        /// <summary>
+        /// Observable上で起きたすべてのイベントをログに出力する.
+        /// </summary>
+        /// <param name="source">任意のObservable.</param>
+        /// <param name="onNext">ログに出力したい値を取得する処理.</param>
+        /// <typeparam name="T">Observableの型.</typeparam>
+        /// <typeparam name="TState">ログに出力したい値の型.</typeparam>
+        /// <returns>ログ出力を行うObservable.</returns>
+        public static Observable<T> Debug<T, TState>(this Observable<T> source, Func<T, TState> onNext)
+        {
+#if UNITY_EDITOR || DEVELOPMENT_BUILD
+            return source
+                .Do(
+                    state: onNext,
+                    onNext: static (x, state) => UnityEngine.Debug.Log($"OnNext({state(x)})"),
+                    onErrorResume: static (x, _) => UnityEngine.Debug.Log($"OnErrorResume({x})"),
+                    onCompleted: static (x, _) => UnityEngine.Debug.Log($"OnCompleted({x})"),
+                    onSubscribe: static _ => UnityEngine.Debug.Log("OnSubscribe"),
+                    onDispose: static _ => UnityEngine.Debug.Log("OnDispose"));
 #else
             return source;
 #endif
