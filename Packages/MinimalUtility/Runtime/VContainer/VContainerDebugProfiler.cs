33518f7aad4bd5731fbc1b8f0111458fcd6a6a55
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-05-28 00:13:06 +0900

    Fix to use UniTask's AsyncGUITrigger

--- Packages/MinimalUtility/Runtime/VContainer/VContainerDebugProfiler.cs
+++ Packages/MinimalUtility/Runtime/VContainer/VContainerDebugProfiler.cs
@@ -1,4 +1,4 @@
-﻿#if ENABLE_VCONTAINER && ENABLE_R3
+﻿#if ENABLE_VCONTAINER && ENABLE_R3 && ENABLE_UNITASK
 using VContainer.Unity;
 
 namespace MinimalUtility.VContainer
@@ -8,7 +8,7 @@ namespace MinimalUtility.VContainer
     /// <summary>
     /// <see cref="IStartable"/>のエントリーポイントを持つデバッグプロファイラー.
     /// </summary>
-    public sealed class VContainerDebugProfiler : DebugProfiler, IStartable
+    public sealed class VContainerDebugProfiler : DebugProfiler, IAsyncStartable
     {
         private const MemoryUnit DefaultUnit = MemoryUnit.GB;
 
