eb6063ccd40c8ab84918204a62c9c4dddb146192
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-06-22 00:51:30 +0900

    Add EnumStringConverter

--- SourceGenerator/MinimalUtility.SourceGenerator/EnumStringConverter.cs
+++ SourceGenerator/MinimalUtility.SourceGenerator/EnumStringConverter.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.CodeAnalysis;
+﻿using System.Text;
+using Microsoft.CodeAnalysis;
 
 namespace MinimalUtility.SourceGenerator;
 
@@ -10,7 +11,7 @@ public sealed class EnumStringConverter : IIncrementalGenerator
         // 属性クラスの出力
         context.RegisterPostInitializationOutput(static context =>
         {
-            context.AddSource("GenerateStringConverterAttribute.g.cs", @"
+            context.AddSource("GenerateStringConverterAttribute.g.cs", """
             using System;
 
             namespace MinimalUtility.SourceGenerator
@@ -20,7 +21,7 @@ public sealed class EnumStringConverter : IIncrementalGenerator
                 {
                 }
             }
-            ");
+            """);
         });
 
         var source = context.SyntaxProvider.ForAttributeWithMetadataName(
@@ -40,6 +41,105 @@ public sealed class EnumStringConverter : IIncrementalGenerator
                 string baseType = typeSymbol.GetEnumBaseTypeStr();
 
                 string fullType = typeSymbol.GetFullTypeName();
+
+                bool isGlobalNamespace = typeSymbol.ContainingNamespace.IsGlobalNamespace;
+
+                List<string> values = new ();
+                StringBuilder builder = new ();
+
+                builder.Append(isGlobalNamespace ?
+                    $$"""
+                    public partial class {{targetTypeName}}StringConverter
+                    {
+                        private readonly string[] values = new string[]
+                        {
+                    """ :
+                    $$"""
+                    namespace {{typeSymbol.ContainingNamespace}}
+                    {
+                        public partial class {{targetTypeName}}StringConverter
+                        {
+                            private readonly string[] values = new string[]
+                            {
+                    """);
+                builder.AppendLine();
+
+                foreach (ISymbol member in typeSymbol.GetMembers())
+                {
+                    foreach (AttributeData attribute in member.GetAttributes())
+                    {
+                        if (attribute.AttributeClass?.Name != "EnumMemberAttribute") continue;
+                        foreach (KeyValuePair<string, TypedConstant> argument in attribute.NamedArguments)
+                        {
+                            if (argument.Key != "Value") continue;
+                            string? enumMemberValue = argument.Value.Value?.ToString();
+                            if (enumMemberValue is null) continue;
+                            builder.Append(isGlobalNamespace ? "        \"" : "            \"");
+                            builder.Append(enumMemberValue);
+                            builder.AppendLine("\",");
+
+                            if (member is not IFieldSymbol fieldSymbol) continue;
+                            string? value = fieldSymbol.ConstantValue?.ToString();
+                            if (value is not null)
+                            {
+                                values.Add(value);
+                            }
+                        }
+                    }
+                }
+
+                values.TrimExcess();
+
+                builder.AppendLine(isGlobalNamespace ? "    };" : "        };");
+                builder.AppendLine();
+                builder.AppendLine(isGlobalNamespace ? $$"""
+                        public System.ReadOnlySpan<string> MemberValues => values;
+                    
+                        public ref readonly string Convert(in {{targetTypeName}} value)
+                        {
+                            switch (({{baseType}})value)
+                            {
+                    """ :
+                    $$"""
+                            public System.ReadOnlySpan<string> MemberValues => values;
+                    
+                            public ref readonly string Convert(in {{targetTypeName}} value)
+                            {
+                                switch (({{baseType}})value)
+                                {
+                    """);
+
+                for (int i = 0; i < values.Count; i++)
+                {
+                    builder.AppendLine(isGlobalNamespace ?$"""
+                                    case {values[i]}:
+                                        return ref values[{i}];
+                        """ :
+                        $"""
+                                        case {values[i]}:
+                                            return ref values[{i}];
+                        """);
+                }
+
+                values.Clear();
+
+                builder.AppendLine(isGlobalNamespace ? """
+                                default:
+                                    return ref string.Empty;
+                            }
+                        }
+                    }
+                    """ :
+                    """
+                                    default:
+                                        return ref string.Empty;
+                                }
+                            }
+                        }
+                    }
+                    """);
+
+                context.AddSource($"{fullType}.StringConverter.g.cs", builder.ToString());
             });
     }
 }
