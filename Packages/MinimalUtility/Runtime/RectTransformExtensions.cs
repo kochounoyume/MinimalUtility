f5dd2250c543b447f18587e48ce17f65ec8c3053
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-06-23 15:55:48 +0900

    Update RectTransformExtensions

--- Packages/MinimalUtility/Runtime/RectTransformExtensions.cs
+++ Packages/MinimalUtility/Runtime/RectTransformExtensions.cs
@@ -12,15 +12,47 @@ namespace MinimalUtility
         /// <see cref="RectTransform.sizeDelta"/>よりも安全なサイズ設定.
         /// </summary>
         /// <param name="target">対象の<see cref="RectTransform"/>.</param>
-        /// <param name="sizeX">横の長さ.</param>
-        /// <param name="sizeY">縦の長さ.</param>
+        /// <param name="width">横の長さ.</param>
+        /// <param name="height">縦の長さ.</param>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void SetSafeSize(this RectTransform target, in float sizeX, in float sizeY)
+        public static void SetSafeSize(this RectTransform target, in float width, in float height)
         {
             const RectTransform.Axis horizontal = RectTransform.Axis.Horizontal;
-            target.SetSizeWithCurrentAnchors(horizontal, sizeX);
+            target.SetSizeWithCurrentAnchors(horizontal, width);
             const RectTransform.Axis vertical = RectTransform.Axis.Vertical;
-            target.SetSizeWithCurrentAnchors(vertical, sizeY);
+            target.SetSizeWithCurrentAnchors(vertical, height);
         }
+
+        /// <summary>
+        /// <see cref="RectTransform.sizeDelta"/>よりも安全な横幅設定.
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <param name="width">横の長さ.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetSafeSizeWidth(this RectTransform target, in float width)
+        {
+            const RectTransform.Axis horizontal = RectTransform.Axis.Horizontal;
+            target.SetSizeWithCurrentAnchors(horizontal, width);
+        }
+
+        /// <summary>
+        /// <see cref="RectTransform.sizeDelta"/>よりも安全な縦幅設定.
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <param name="height">縦の長さ.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void SetSafeSizeHeight(this RectTransform target, in float height)
+        {
+            const RectTransform.Axis vertical = RectTransform.Axis.Vertical;
+            target.SetSizeWithCurrentAnchors(vertical, height);
+        }
+
+        /// <summary>
+        /// <see cref="RectTransform.sizeDelta"/>よりも安全なサイズ取得.
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <returns>対象のサイズ.</returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static Vector2 GetSizeDelta(this RectTransform target) => target.rect.size;
     }
 }
