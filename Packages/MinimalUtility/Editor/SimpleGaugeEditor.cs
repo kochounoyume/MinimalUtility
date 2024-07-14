8c0f2d0d4e524f83104800cf2dd3abcb4cc7b5e7
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-07-15 00:40:57 +0900

    Add SimpleGauge and remove SimpleSliceGauge

---
+++ Packages/MinimalUtility/Editor/SimpleGaugeEditor.cs
@@ -0,0 +1,29 @@
+﻿using UnityEditor;
+
+namespace MinimalUtility.Editor
+{
+    /// <summary>
+    /// <see cref="SimpleGauge"/>のカスタムエディタ.
+    /// </summary>
+    [CustomEditor(typeof(SimpleGauge))]
+    public class SimpleGaugeEditor : UnityEditor.Editor
+    {
+        private SerializedProperty mode;
+        private SerializedProperty value;
+
+        /// <inheritdoc/>
+        public override void OnInspectorGUI()
+        {
+            serializedObject.Update();
+            EditorGUILayout.PropertyField(mode);
+            EditorGUILayout.PropertyField(value);
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void OnEnable()
+        {
+            mode = serializedObject.FindProperty("mode");
+            value = serializedObject.FindProperty("value");
+        }
+    }
+}
---
+++ Packages/MinimalUtility/Runtime/SimpleGauge.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace MinimalUtility
+{
+    /// <summary>
+    /// <see cref="RectMask2D"/>を継承利用したシンプルで綺麗なゲージ表示.
+    /// </summary>
+    [RequireComponent(typeof(MaskableGraphic))]
+    public class SimpleGauge : RectMask2D
+    {
+        [SerializeField]
+        [Tooltip("ゲージの表示方向")]
+        private RectTransform.Edge mode = RectTransform.Edge.Right;
+
+        [SerializeField]
+        [Tooltip("ゲージの値(0.0 ～ 1.0)")]
+        [Range(0, 1)]
+        private float value = 1.0f;
+
+        /// <summary>
+        /// ゲージの値(0.0 ～ 1.0).
+        /// </summary>
+        public float Value
+        {
+            get => value;
+            set
+            {
+                this.value = Mathf.Clamp01(value);
+                var currentPadding = padding;
+                switch (mode)
+                {
+                    case RectTransform.Edge.Left:
+                        currentPadding.x = rectTransform.rect.width * (1.0f - this.value);
+                        break;
+                    case RectTransform.Edge.Right:
+                        currentPadding.z = rectTransform.rect.width * (1.0f - this.value);
+                        break;
+                    case RectTransform.Edge.Top:
+                        currentPadding.w = rectTransform.rect.height * (1.0f - this.value);
+                        break;
+                    case RectTransform.Edge.Bottom:
+                        currentPadding.y = rectTransform.rect.height * (1.0f - this.value);
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(Value));
+                }
+                padding = currentPadding;
+            }
+        }
+
+        private MaskableGraphic graphic;
+
+        /// <inheritdoc/>
+        protected override void OnEnable()
+        {
+            base.OnEnable();
+            if (graphic == null)
+            {
+                graphic = GetComponent<MaskableGraphic>();
+            }
+            AddClippable(graphic);
+        }
+
+#if UNITY_EDITOR
+        /// <inheritdoc/>
+        protected override void OnValidate()
+        {
+            Value = value;
+            base.OnValidate();
+        }
+#endif
+    }
+}
