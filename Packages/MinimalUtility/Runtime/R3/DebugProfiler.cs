3e02b87e684f228c98941002bb4aacf441d98d2d
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-07-14 23:25:05 +0900

    Modified for MemoryUnit to be nested in DebugProfiler

--- Packages/MinimalUtility/Runtime/R3/DebugProfiler.cs
+++ Packages/MinimalUtility/Runtime/R3/DebugProfiler.cs
@@ -20,29 +20,6 @@ namespace MinimalUtility.R3
     using SourceGenerator;
 
     /// <summary>
-    /// 総メモリ使用量表示の単位指定列挙体.
-    /// </summary>
-    [GenerateStringConverter]
-    public enum MemoryUnit : int
-    {
-        /// <summary>バイト</summary>
-        [EnumMember(Value = "B")]
-        B = 0,
-
-        /// <summary>キロバイト</summary>
-        [EnumMember(Value = "KB")]
-        KB = 1,
-
-        /// <summary>メガバイト</summary>
-        [EnumMember(Value = "MB")]
-        MB = 2,
-
-        /// <summary>ギガバイト</summary>
-        [EnumMember(Value = "GB")]
-        GB = 3
-    }
-
-    /// <summary>
     /// FPSなどのプロファイル情報を画面に表示するためのクラス.
     /// <remarks>
     /// <see cref="Time.realtimeSinceStartup"/> の公式リファレンスサンプルコードを参照
@@ -50,6 +27,29 @@ namespace MinimalUtility.R3
     /// </summary>
     public class DebugProfiler
     {
+        /// <summary>
+        /// 総メモリ使用量表示の単位指定列挙体.
+        /// </summary>
+        [GenerateStringConverter]
+        public enum MemoryUnit : int
+        {
+            [InspectorName("バイト")]
+            [EnumMember(Value = nameof(B))]
+            B = 0,
+
+            [InspectorName("キロバイト")]
+            [EnumMember(Value = nameof(KB))]
+            KB = 1,
+
+            [InspectorName("メガバイト")]
+            [EnumMember(Value = nameof(MB))]
+            MB = 2,
+
+            [InspectorName("ギガバイト")]
+            [EnumMember(Value = nameof(GB))]
+            GB = 3
+        }
+
         /// <summary>
         /// プロファイル情報を更新する間隔.
         /// </summary>
@@ -80,9 +80,8 @@ namespace MinimalUtility.R3
         /// プロファイル情報の表示を開始する.
         /// </summary>
         /// <param name="cancellation">キャンセルトークン.</param>
-#pragma warning disable SA1615 // ElementReturnValueMustBeDocumented
+        /// <returns>UniTask.</returns>
         public async UniTask StartAsync(CancellationToken cancellation)
-#pragma warning restore SA1615 // ElementReturnValueMustBeDocumented
         {
             TimeSpan interval = TimeSpan.FromSeconds(IntervalSecs);
             StringBuilder sb = new StringBuilder();
@@ -90,34 +89,27 @@ namespace MinimalUtility.R3
             Observable.Timer(interval, interval, UnityTimeProvider.UpdateRealtime, cancellation)
                 .Select(UnityFrameProvider.Update, static (_, provider) => provider.GetFrameCount())
                 .Pairwise()
-                .Subscribe(new { sb, unit = memoryUnit, unitStr = memoryUnitStringConverter.Convert(memoryUnit) },
+                .Subscribe((sb, unit: memoryUnit, unitStr: memoryUnitStringConverter.Convert(memoryUnit)),
                     static (pair, param) =>
                     {
                         long count = pair.Current - pair.Previous;
-                        param.sb.Clear();
-                        const string cpuText = "CPU: ";
-                        param.sb.Append(cpuText);
                         float fps = count / IntervalSecs;
-                        const string formatF0 = "F0";
-                        param.sb.Append(fps.ToString(formatF0));
-                        const string fpsText = "fps (";
-                        param.sb.Append(fpsText);
                         float ms = IntervalSecs / count * 1000f;
-                        const string formatF1 = "F1";
-                        param.sb.Append(ms.ToString(formatF1));
-                        const string msText = "ms)";
-                        param.sb.AppendLine(msText);
-                        const string memoryText = "Memory: ";
-                        param.sb.Append(memoryText);
                         // 確保している総メモリ
                         float totalMemory = Profiler.GetTotalReservedMemoryLong() / Mathf.Pow(1024f, (int)param.unit);
-                        const string formatF = "F";
-                        param.sb.Append(totalMemory.ToString(formatF));
-                        param.sb.Append(param.unitStr);
+                        param.sb.Clear();
+                        param.sb
+                            .Append("CPU: ")
+                            .Append(fps.ToString("F0"))
+                            .Append("fps (")
+                            .Append(ms.ToString("F1"))
+                            .AppendLine("ms)")
+                            .Append("Memory: ")
+                            .Append(totalMemory.ToString("F"))
+                            .Append(param.unitStr);
                     });
 
-            const string instanceName = "DebugProfiler";
-            GameObject instanceObj = new GameObject(instanceName, typeof(AsyncGUITrigger));
+            GameObject instanceObj = new GameObject("DebugProfiler", typeof(AsyncGUITrigger));
             // 破壊されないように
             Object.DontDestroyOnLoad(instanceObj);
 
