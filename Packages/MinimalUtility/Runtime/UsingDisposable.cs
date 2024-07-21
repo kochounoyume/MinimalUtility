12696a0c21b575b2bd59d114ddd19a568b7e4743
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-07-21 17:20:45 +0900

    Rename SloppyDisposable to UsingDisposable and Fix "readonly ref struct" to "readonly struct"

--- Packages/MinimalUtility/Runtime/UsingDisposable.cs
+++ Packages/MinimalUtility/Runtime/UsingDisposable.cs
@@ -5,15 +5,15 @@ namespace MinimalUtility
     /// <summary>
     /// usingステートメントスコープを抜けた際にコールバックを呼び出すための構造体.
     /// </summary>
-    public readonly ref struct SloppyDisposable
+    public readonly struct UsingDisposable : IDisposable
     {
         private readonly Action onDispose;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SloppyDisposable"/> struct.
+        /// Initializes a new instance of the <see cref="UsingDisposable"/> struct.
         /// </summary>
         /// <param name="onDispose">破棄時に呼び出す処理.</param>
-        public SloppyDisposable(Action onDispose)
+        public UsingDisposable(Action onDispose)
         {
             this.onDispose = onDispose;
         }
@@ -21,7 +21,7 @@ namespace MinimalUtility
         /// <summary>
         /// 破棄処理.
         /// </summary>
-        public void Dispose()
+        void IDisposable.Dispose()
         {
             onDispose?.Invoke();
         }
@@ -31,17 +31,17 @@ namespace MinimalUtility
     /// usingステートメントスコープを抜けた際にコールバックを呼び出すための構造体.
     /// </summary>
     /// <typeparam name="T0">破棄時に呼び出す処理の引数の型.</typeparam>
-    public readonly ref struct SloppyDisposable<T0>
+    public readonly struct UsingDisposable<T0> : IDisposable
     {
         private readonly T0 state0;
         private readonly Action<T0> onDispose;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SloppyDisposable{T}"/> struct.
+        /// Initializes a new instance of the <see cref="UsingDisposable{T}"/> struct.
         /// </summary>
         /// <param name="state0">破棄時に呼び出す処理の引数.</param>
         /// <param name="onDispose">破棄時に呼び出す処理.</param>
-        public SloppyDisposable(T0 state0, Action<T0> onDispose)
+        public UsingDisposable(T0 state0, Action<T0> onDispose)
         {
             this.state0 = state0;
             this.onDispose = onDispose;
@@ -50,7 +50,7 @@ namespace MinimalUtility
         /// <summary>
         /// 破棄処理.
         /// </summary>
-        public void Dispose()
+        void IDisposable.Dispose()
         {
             onDispose?.Invoke(state0);
         }
@@ -61,19 +61,19 @@ namespace MinimalUtility
     /// </summary>
     /// <typeparam name="T0">破棄時に呼び出す処理の第一引数の型.</typeparam>
     /// <typeparam name="T1">破棄時に呼び出す処理の第二引数の型.</typeparam>
-    public readonly ref struct SloppyDisposable<T0, T1>
+    public readonly struct UsingDisposable<T0, T1> : IDisposable
     {
         private readonly T0 state0;
         private readonly T1 state1;
         private readonly Action<T0, T1> onDispose;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SloppyDisposable{T0, T1}"/> struct.
+        /// Initializes a new instance of the <see cref="UsingDisposable{T0, T1}"/> struct.
         /// </summary>
         /// <param name="state0">破棄時に呼び出す処理の第一引数.</param>
         /// <param name="state1">破棄時に呼び出す処理の第二引数.</param>
         /// <param name="onDispose">破棄時に呼び出す処理.</param>
-        public SloppyDisposable(T0 state0, T1 state1, Action<T0, T1> onDispose)
+        public UsingDisposable(T0 state0, T1 state1, Action<T0, T1> onDispose)
         {
             this.state0 = state0;
             this.state1 = state1;
@@ -83,7 +83,7 @@ namespace MinimalUtility
         /// <summary>
         /// 破棄処理.
         /// </summary>
-        public void Dispose()
+        void IDisposable.Dispose()
         {
             onDispose?.Invoke(state0, state1);
         }
@@ -95,7 +95,7 @@ namespace MinimalUtility
     /// <typeparam name="T0">破棄時に呼び出す処理の第一引数の型.</typeparam>
     /// <typeparam name="T1">破棄時に呼び出す処理の第二引数の型.</typeparam>
     /// <typeparam name="T2">破棄時に呼び出す処理の第三引数の型.</typeparam>
-    public readonly ref struct SloppyDisposable<T0, T1, T2>
+    public readonly struct UsingDisposable<T0, T1, T2> : IDisposable
     {
         private readonly T0 state0;
         private readonly T1 state1;
@@ -103,13 +103,13 @@ namespace MinimalUtility
         private readonly Action<T0, T1, T2> onDispose;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="SloppyDisposable{T0, T1, T2}"/> struct.
+        /// Initializes a new instance of the <see cref="UsingDisposable{T0, T1, T2}"/> struct.
         /// </summary>
         /// <param name="state0">破棄時に呼び出す処理の第一引数.</param>
         /// <param name="state1">破棄時に呼び出す処理の第二引数.</param>
         /// <param name="state2">破棄時に呼び出す処理の第三引数.</param>
         /// <param name="onDispose">破棄時に呼び出す処理.</param>
-        public SloppyDisposable(T0 state0, T1 state1, T2 state2, Action<T0, T1, T2> onDispose)
+        public UsingDisposable(T0 state0, T1 state1, T2 state2, Action<T0, T1, T2> onDispose)
         {
             this.state0 = state0;
             this.state1 = state1;
@@ -120,7 +120,7 @@ namespace MinimalUtility
         /// <summary>
         /// 破棄処理.
         /// </summary>
-        public void Dispose()
+        void IDisposable.Dispose()
         {
             onDispose?.Invoke(state0, state1, state2);
         }
