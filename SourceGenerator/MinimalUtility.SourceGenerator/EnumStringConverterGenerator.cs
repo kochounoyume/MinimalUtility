57cbfe6bff37b95b42fa88eeaee9dedfb134e611
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-07-27 09:45:54 +0900

    Change to method using Collect

--- SourceGenerator/MinimalUtility.SourceGenerator/EnumStringConverterGenerator.cs
+++ SourceGenerator/MinimalUtility.SourceGenerator/EnumStringConverterGenerator.cs
@@ -14,158 +14,170 @@ internal sealed class EnumStringConverterGenerator : IIncrementalGenerator
             AttributeName,
             static (_, _) => true,
             static (context, _) => context
-        );
+        ).Collect();
 
         context.RegisterSourceOutput(
             source,
             static (context, metaDataArray) =>
             {
-                var typeSymbol = (INamedTypeSymbol)metaDataArray.TargetSymbol;
-                string targetTypeName = typeSymbol.Name;
-                string targetNestedTypeName = typeSymbol.GetNestedName();
-                string baseType = typeSymbol.GetEnumBaseTypeStr();
-                string fullType = typeSymbol.GetFullTypeName();
-                bool isGlobalNamespace = typeSymbol.ContainingNamespace.IsGlobalNamespace;
-
-                bool autoGenerate = metaDataArray.Attributes[0].ConstructorArguments[0].Value?.ToString() == "True";
-
-                List<string> values = new ();
-                List<string> fieldNames = new ();
-                StringBuilder builder = new ();
-
-                builder.Append(isGlobalNamespace ?
-                    $$"""
-                    using System;
-                    
-                    public partial class {{targetTypeName}}StringConverter
-                    {
-                        private readonly string[] values = new string[]
-                        {
-                    """ :
-                    $$"""
-                    using System;
-                    
-                    namespace {{typeSymbol.ContainingNamespace}}
-                    {
-                        public partial class {{targetTypeName}}StringConverter
-                        {
-                            private readonly string[] values = new string[]
-                            {
-                    """);
-                builder.AppendLine();
-
-                foreach (ISymbol member in typeSymbol.GetMembers())
+                foreach (var syntaxContext in metaDataArray)
                 {
-                    bool shouldAutoGenerate = autoGenerate && member.Kind == SymbolKind.Field;
-                    foreach (AttributeData attribute in member.GetAttributes())
+                    var typeSymbol = (INamedTypeSymbol)syntaxContext.TargetSymbol;
+                    string targetTypeName = typeSymbol.Name;
+                    string targetNestedTypeName = typeSymbol.GetNestedName();
+                    string baseType = typeSymbol.GetEnumBaseTypeStr();
+                    string fullType = typeSymbol.GetFullTypeName();
+                    bool isGlobalNamespace = typeSymbol.ContainingNamespace.IsGlobalNamespace;
+
+                    bool autoGenerate = syntaxContext.Attributes[0].ConstructorArguments[0].Value?.ToString() == "True";
+
+                    List<string> values = new ();
+                    List<string> fieldNames = new ();
+                    StringBuilder builder = new ();
+
+                    builder.Append(isGlobalNamespace ?
+                        $$"""
+                          using System;
+
+                          public partial class {{targetTypeName}}StringConverter
+                          {
+                              private readonly string[] values = new string[]
+                              {
+                          """ :
+                        $$"""
+                          using System;
+
+                          namespace {{typeSymbol.ContainingNamespace}}
+                          {
+                              public partial class {{targetTypeName}}StringConverter
+                              {
+                                  private readonly string[] values = new string[]
+                                  {
+                          """
+                    );
+                    builder.AppendLine();
+
+                    foreach (ISymbol member in typeSymbol.GetMembers())
                     {
-                        if (attribute.AttributeClass?.Name != nameof(System.Runtime.Serialization.EnumMemberAttribute)) continue;
-                        foreach (KeyValuePair<string, TypedConstant> argument in attribute.NamedArguments)
+                        bool shouldAutoGenerate = autoGenerate && member.Kind == SymbolKind.Field;
+                        foreach (AttributeData attribute in member.GetAttributes())
                         {
-                            if (argument.Key != "Value") continue;
-                            string? enumMemberValue = argument.Value.Value?.ToString();
-                            if (enumMemberValue is null) continue;
-                            AddMember(builder, isGlobalNamespace, enumMemberValue, member, values, fieldNames);
-                            shouldAutoGenerate = false;
+                            if (attribute.AttributeClass?.Name != nameof(System.Runtime.Serialization.EnumMemberAttribute)) continue;
+                            foreach (KeyValuePair<string, TypedConstant> argument in attribute.NamedArguments)
+                            {
+                                if (argument.Key != "Value") continue;
+                                string? enumMemberValue = argument.Value.Value?.ToString();
+                                if (enumMemberValue is null) continue;
+                                AddMember(builder, isGlobalNamespace, enumMemberValue, member, values, fieldNames);
+                                shouldAutoGenerate = false;
+                            }
+                        }
+
+                        if (shouldAutoGenerate)
+                        {
+                            AddMember(builder, isGlobalNamespace, member.Name, member, values, fieldNames);
                         }
                     }
 
-                    if (shouldAutoGenerate)
-                    {
-                        AddMember(builder, isGlobalNamespace, member.Name, member, values, fieldNames);
-                    }
-                }
+                    values.TrimExcess();
+                    fieldNames.TrimExcess();
 
-                values.TrimExcess();
-                fieldNames.TrimExcess();
-
-                builder.AppendLine(isGlobalNamespace ? "    };" : "        };");
-                builder.AppendLine();
-                builder.AppendLine(isGlobalNamespace ? $$"""
-                        public ReadOnlySpan<string> MemberValues => values;
-                    
-                        public ref readonly string Convert(in {{targetNestedTypeName}} value)
-                        {
-                            switch (({{baseType}})value)
-                            {
-                    """ :
-                    $$"""
-                            public ReadOnlySpan<string> MemberValues => values;
-                    
-                            public ref readonly string Convert(in {{targetNestedTypeName}} value)
-                            {
-                                switch (({{baseType}})value)
-                                {
-                    """);
-
-                for (int i = 0; i < values.Count; i++)
-                {
-                    builder.AppendLine(isGlobalNamespace ? $"""
-                                    case {values[i]}:
-                                        return ref values[{i}];
-                        """ :
-                        $"""
-                                        case {values[i]}:
-                                            return ref values[{i}];
-                        """);
-                }
-
-                values.Clear();
-
-                builder.AppendLine(isGlobalNamespace ? $$"""
-                                default:
-                                    return ref string.Empty;
-                            }
-                        }
-                        
-                        public {{targetNestedTypeName}} ReverseConvert(in string str)
-                        {
-                            switch (Array.IndexOf(values, str))
-                            {
-                    """ :
-                    $$"""
-                                    default:
-                                        return ref string.Empty;
-                                }
-                            }
+                    builder.AppendLine(isGlobalNamespace ? "    };" : "        };");
+                    builder.AppendLine();
+                    builder.AppendLine(isGlobalNamespace
+                        ? $$"""
+                                public ReadOnlySpan<string> MemberValues => values;
                             
-                            public {{targetNestedTypeName}} ReverseConvert(in string str)
-                            {
-                                switch (Array.IndexOf(values, str))
+                                public ref readonly string Convert(in {{targetNestedTypeName}} value)
                                 {
-                    """);
+                                    switch (({{baseType}})value)
+                                    {
+                            """
+                        : $$"""
+                                    public ReadOnlySpan<string> MemberValues => values;
+                            
+                                    public ref readonly string Convert(in {{targetNestedTypeName}} value)
+                                    {
+                                        switch (({{baseType}})value)
+                                        {
+                            """
+                    );
 
-                for (int i = 0; i < fieldNames.Count; i++)
-                {
-                    builder.AppendLine(isGlobalNamespace ? $"""
-                                    case {i}:
-                                        return {targetNestedTypeName}.{fieldNames[i]};
-                        """ :
-                        $"""
-                                        case {i}:
-                                            return {targetNestedTypeName}.{fieldNames[i]};
-                        """);
-                }
-
-                fieldNames.Clear();
-
-                builder.Append(isGlobalNamespace ? """
-                                default:
-                                    throw new InvalidCastException(str);
-                            }
-                        }
+                    for (int i = 0; i < values.Count; i++)
+                    {
+                        builder.AppendLine(isGlobalNamespace
+                            ? $"""
+                                           case {values[i]}:
+                                               return ref values[{i}];
+                               """
+                            : $"""
+                                               case {values[i]}:
+                                                   return ref values[{i}];
+                               """
+                        );
                     }
-                    """ :
-                    """
-                                    default:
-                                        throw new InvalidCastException(str);
+
+                    values.Clear();
+
+                    builder.AppendLine(isGlobalNamespace
+                        ? $$"""
+                                        default:
+                                            return ref string.Empty;
+                                    }
+                                }
+                                
+                                public {{targetNestedTypeName}} ReverseConvert(in string str)
+                                {
+                                    switch (Array.IndexOf(values, str))
+                                    {
+                            """
+                        : $$"""
+                                            default:
+                                                return ref string.Empty;
+                                        }
+                                    }
+                                    
+                                    public {{targetNestedTypeName}} ReverseConvert(in string str)
+                                    {
+                                        switch (Array.IndexOf(values, str))
+                                        {
+                            """
+                    );
+
+                    for (int i = 0; i < fieldNames.Count; i++)
+                    {
+                        builder.AppendLine(isGlobalNamespace
+                            ? $"""
+                                           case {i}:
+                                               return {targetNestedTypeName}.{fieldNames[i]};
+                               """
+                            : $"""
+                                               case {i}:
+                                                   return {targetNestedTypeName}.{fieldNames[i]};
+                               """
+                        );
+                    }
+
+                    fieldNames.Clear();
+
+                    builder.Append(isGlobalNamespace ? """
+                                                                   default:
+                                                                       throw new InvalidCastException(str);
+                                                               }
+                                                           }
+                                                       }
+                                                       """ :
+                        """
+                                        default:
+                                            throw new InvalidCastException(str);
+                                    }
                                 }
                             }
                         }
-                    }
-                    """);
+                        """);
 
-                context.AddSource($"{fullType}.StringConverter.g.cs", builder.ToString());
+                    context.AddSource($"{fullType}.StringConverter.g.cs", builder.ToString());
+                }
             });
 
         static void AddMember(
