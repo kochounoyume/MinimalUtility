5f1d8816fddfe36b8035291476257e604d678590
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-08-02 00:29:25 +0900

    Add Transform CustomEditor

---
+++ Packages/MinimalUtility/Editor/TransformEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace MinimalUtility.Editor
+{
+    /// <summary>
+    /// <see cref="Transform"/>のカスタムインスペクター.
+    /// </summary>
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(Transform))]
+    internal class TransformEditor : UnityEditor.Editor
+    {
+        /// <inheritdoc/>
+        public override void OnInspectorGUI()
+        {
+            Transform transform = target as Transform;
+            if (transform == null) return;
+            transform.position = EditorGUILayout.Vector3Field("World Position", transform.position);
+            transform.eulerAngles = EditorGUILayout.Vector3Field("World Rotation", transform.rotation.eulerAngles);
+            EditorGUI.BeginDisabledGroup(true);
+            EditorGUILayout.Vector3Field("World Scale", transform.lossyScale);
+            EditorGUI.EndDisabledGroup();
+            base.OnInspectorGUI();
+        }
+    }
+}
