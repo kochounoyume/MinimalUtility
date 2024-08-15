f294c6197ac3d4123e45db944db6004cbdd57fa3
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-08-16 00:09:41 +0900

    Add WaitForEndFrameSafety that is UniTask extension

---
+++ Packages/MinimalUtility/Runtime/UniTaskEx/UniTaskExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System.Runtime.CompilerServices;
+using System.Threading;
+using Cysharp.Threading.Tasks;
+using UnityEngine;
+
+namespace MinimalUtility.UniTaskEx
+{
+    public static class UniTaskExtensions
+    {
+        /// <summary>
+        /// <see cref="UniTask.WaitForEndOfFrame(MonoBehaviour)"/>は内部にコルーチンを含む.
+        /// そのためコルーチンの駆動元である<see cref="MonoBehaviour"/>が非表示になるとエラーが発生する.
+        /// それを回避したうえで安全にフレーム終わりまで待機する.
+        /// </summary>
+        /// <param name="monoBehaviour"></param>
+        /// <param name="cancelImmediately"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async UniTask WaitForEndFrameSafety(this MonoBehaviour monoBehaviour, bool cancelImmediately = false)
+        {
+            await UniTask.WaitUntil(
+                monoBehaviour,
+                static m => m.isActiveAndEnabled,
+                cancellationToken: monoBehaviour.destroyCancellationToken,
+                cancelImmediately: cancelImmediately);
+            await UniTask.WaitForEndOfFrame(monoBehaviour, monoBehaviour.destroyCancellationToken, cancelImmediately);
+        }
+
+        /// <summary>
+        /// <see cref="UniTask.WaitForEndOfFrame(MonoBehaviour)"/>は内部にコルーチンを含む.
+        /// そのためコルーチンの駆動元である<see cref="MonoBehaviour"/>が非表示になるとエラーが発生する.
+        /// それを回避したうえで安全にフレーム終わりまで待機する.
+        /// </summary>
+        /// <param name="monoBehaviour"></param>
+        /// <param name="cancellationToken"></param>
+        /// <param name="cancelImmediately"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static async UniTask WaitForEndFrameSafety(this MonoBehaviour monoBehaviour, CancellationToken cancellationToken, bool cancelImmediately = false)
+        {
+            await UniTask.WaitUntil(
+                monoBehaviour,
+                static m => m.isActiveAndEnabled,
+                cancellationToken: cancellationToken,
+                cancelImmediately: cancelImmediately);
+            await UniTask.WaitForEndOfFrame(monoBehaviour, cancellationToken, cancelImmediately);
+        }
+    }
+}
