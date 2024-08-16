ec0c4c0017ab581317c8c850071ccd30a445e193
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-08-17 00:03:03 +0900

    Update comment

--- Packages/MinimalUtility/Runtime/MultiLibraries/DebugProfiler.VContainer.cs
+++ Packages/MinimalUtility/Runtime/MultiLibraries/DebugProfiler.VContainer.cs
@@ -3,6 +3,9 @@ using VContainer.Unity;
 
 namespace MinimalUtility.MultiLibraries
 {
+    /// <summary>
+    /// <see cref="IAsyncStartable"/>を実装するための分割部分.
+    /// </summary>
     public sealed partial class DebugProfiler : IAsyncStartable
     {
     }
