2f3bf9208d896bb239c59a0c40ef4c1252235f68
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-08-21 02:03:36 +0900

    Add MeshRendererEditor

---
+++ Packages/MinimalUtility/Editor/References/MeshRendererEditor.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace UnityEditor.UI
+{
+    [CanEditMultipleObjects]
+    [CustomEditor(typeof(MeshRenderer))]
+    internal class MeshRendererEditor : UnityEditor.MeshRendererEditor
+    {
+        public override void OnInspectorGUI()
+        {
+            base.OnInspectorGUI();
+            Other2DSettingsGUI();
+            serializedObject.ApplyModifiedProperties();
+        }
+    }
+}
