7abe08066ca7301bafbc162e89d229548e0a0c6e
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2024-08-27 00:56:11 +0900

    Organized DebugProfiler and split it into FrameDataProvider and DebugProfileViewer

---
+++ Packages/MinimalUtility/Runtime/DebugProfileViewer.cs
@@ -0,0 +1,126 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace MinimalUtility
+{
+    /// <summary>
+    /// プロファイル情報を表示するためのクラス.
+    /// </summary>
+    [Unity.Profiling.IgnoredByDeepProfiler]
+    public partial class DebugProfileViewer : FrameDataProvider
+    {
+        /// <summary>
+        /// 総メモリ使用量表示の単位指定列挙体.
+        /// </summary>
+        [GenerateStringConverter(true)]
+        public enum MemoryUnit : int
+        {
+            [InspectorName("バイト")]
+            B = 0,
+
+            [InspectorName("キロバイト")]
+            KB = 1,
+
+            [InspectorName("メガバイト")]
+            MB = 2,
+
+            [InspectorName("ギガバイト")]
+            GB = 3
+        }
+
+        /// <summary>
+        /// OnGUIイベントを取得するトリガークラス.
+        /// </summary>
+        [DisallowMultipleComponent]
+        protected class OnGUITrigger : EmptyMonoBehaviour
+        {
+            /// <summary>
+            /// OnGUIのタイミングで呼ばれるイベント.
+            /// </summary>
+            public event Action<FrameTiming> OnGUIEvent;
+
+            /// <summary>
+            /// イベントの引数として渡される、フレームタイミング情報.
+            /// </summary>
+            public FrameTiming State { get; set; }
+
+            private void OnGUI() => OnGUIEvent?.Invoke(State);
+        }
+
+        private const float IntervalSecs = 0.5f;
+        private readonly Lazy<WaitForSeconds> intervalWait = new (static () => new WaitForSeconds(IntervalSecs));
+        private readonly Lazy<OnGUITrigger> onGUITrigger;
+        private readonly MemoryUnitStringConverter memoryUnitStringConverter = new ();
+        // GUIStyleは生成タイミングがOnGUIの中でないとエラーになるため、Lazyで遅延生成
+        private readonly Lazy<GUIStyle> styleBox = new (static () => new GUIStyle(GUI.skin.box)
+        {
+            fontSize = 30,
+            normal = { textColor = Color.white },
+            alignment = TextAnchor.UpperLeft
+        });
+
+        /// <summary>
+        /// 表示する総メモリ使用量の単位.
+        /// </summary>
+        public MemoryUnit Unit { get; set; } = MemoryUnit.GB;
+
+        private bool isGUIVisible = false;
+
+        /// <summary>
+        /// GUI表示の可視状態.
+        /// </summary>
+        public bool IsGUIVisible
+        {
+            get => isGUIVisible;
+            set
+            {
+                var before = isGUIVisible;
+                isGUIVisible = value;
+                if (value && !before)
+                {
+                    onGUITrigger.Value.OnGUIEvent += UpdateGUI;
+                    CoroutineRunner.Value.StartCoroutine(UpdateGUIInterval());
+                }
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DebugProfileViewer"/> class.
+        /// </summary>
+        public DebugProfileViewer() : base()
+        {
+            onGUITrigger = new Lazy<OnGUITrigger>(Cast);
+
+            OnGUITrigger Cast() => CoroutineRunner.Value as OnGUITrigger;
+        }
+
+        /// <inheritdoc/>
+        protected override EmptyMonoBehaviour GetCoroutineRunner()
+        {
+            var go = new GameObject(RunnerName, typeof(OnGUITrigger));
+            UnityEngine.Object.DontDestroyOnLoad(go);
+            return go.GetComponent<OnGUITrigger>();
+        }
+
+        private IEnumerator UpdateGUIInterval()
+        {
+            while (isGUIVisible && CoroutineRunner.Value != null)
+            {
+                onGUITrigger.Value.State = LatestFrameData;
+                yield return intervalWait.Value;
+            }
+        }
+
+        private void UpdateGUI(FrameTiming latest)
+        {
+            double ms = latest.cpuFrameTime;
+            double fps = 1000 / ms;
+            // 確保している総メモリ
+            float totalMemory = UnityEngine.Profiling.Profiler.GetTotalReservedMemoryLong() / Mathf.Pow(1024f, (int)Unit);
+            string text = $"CPU: {fps:F0}fps ({ms:F1}ms){Environment.NewLine}Memory: {totalMemory:F}{memoryUnitStringConverter.Convert(Unit)}";
+            Rect rect = new Rect(Screen.safeArea.position, new Vector2(500, 80));
+            GUI.Box(rect, text, styleBox.Value);
+        }
+    }
+}
---
+++ Packages/MinimalUtility/Runtime/FrameDataProvider.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace MinimalUtility
+{
+    /// <summary>
+    /// フレームデータを提供するクラス.
+    /// </summary>
+    [Unity.Profiling.IgnoredByDeepProfiler]
+    public class FrameDataProvider
+    {
+        /// <summary>
+        /// 空のMonoBehaviour.
+        /// </summary>
+        [DisallowMultipleComponent]
+        protected class EmptyMonoBehaviour : MonoBehaviour
+        {
+        }
+
+        /// <summary>
+        /// ランナーゲームオブジェクトの名前.
+        /// </summary>
+        protected const string RunnerName = "Debug Profiler";
+
+        /// <summary>
+        /// コルーチンを実行するための<see cref="MonoBehaviour"/>の遅延生成.
+        /// </summary>
+        protected readonly Lazy<EmptyMonoBehaviour> CoroutineRunner;
+
+        private readonly FrameTiming[] frameTiming = new FrameTiming[1];
+
+        /// <summary>
+        /// フレームタイミング情報.
+        /// </summary>
+        public ref readonly FrameTiming LatestFrameData
+        {
+            get
+            {
+                if (CoroutineRunner.IsValueCreated)
+                {
+                    CoroutineRunner.Value.StartCoroutine(UpdateFrameTiming());
+                }
+                return ref frameTiming[0];
+            }
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="FrameDataProvider"/> class.
+        /// </summary>
+        public FrameDataProvider()
+        {
+            CoroutineRunner = new Lazy<EmptyMonoBehaviour>(GetCoroutineRunner);
+        }
+
+        /// <summary>
+        /// コルーチンを実行するための<see cref="MonoBehaviour"/>を取得します.
+        /// </summary>
+        /// <returns>コルーチンを実行するための<see cref="MonoBehaviour"/>.</returns>
+        protected virtual EmptyMonoBehaviour GetCoroutineRunner()
+        {
+            var go = new GameObject(RunnerName, typeof(EmptyMonoBehaviour));
+            UnityEngine.Object.DontDestroyOnLoad(go);
+            return go.GetComponent<EmptyMonoBehaviour>();
+        }
+
+        private IEnumerator UpdateFrameTiming()
+        {
+            while (CoroutineRunner.Value != null)
+            {
+                FrameTimingManager.CaptureFrameTimings();
+                if (FrameTimingManager.GetLatestTimings(1, frameTiming) == 0)
+                {
+                    frameTiming[0] = default;
+                }
+                yield return null;
+            }
+        }
+    }
+}
