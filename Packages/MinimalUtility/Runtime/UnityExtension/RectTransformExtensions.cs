94814844d793c1baf602d57841eb40a5bce6e1ec
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-10-24 00:39:49 +0900

    Add GetWorldCorners and GetLocalCorners in RectTransformExtensions

--- Packages/MinimalUtility/Runtime/UnityExtension/RectTransformExtensions.cs
+++ Packages/MinimalUtility/Runtime/UnityExtension/RectTransformExtensions.cs
@@ -1,4 +1,5 @@
-﻿using System.Runtime.CompilerServices;
+﻿using System;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
 namespace MinimalUtility
@@ -61,7 +62,7 @@ namespace MinimalUtility
         /// <param name="target">対象の<see cref="RectTransform"/>.</param>
         /// <returns>対象のサイズ.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Vector2 GetSizeDelta(this RectTransform target) => target.rect.size;
+        public static Vector2 GetSize(this RectTransform target) => target.rect.size;
 
         /// <summary>
         /// <see cref="RectTransform"/>を全面的に伸ばす(stretch * stretchにする).
@@ -83,6 +84,72 @@ namespace MinimalUtility
             target.anchorMax = Vector2.one;
             target.offsetMin = new Vector2(left, bottom);
             target.offsetMax = new Vector2(-right, -top);
+        }
+
+        /// <summary>
+        /// ワールドスペースで計算された長方形の角を取得する.
+        /// <remarks>
+        /// <see cref="RectTransform.GetWorldCorners(Vector3[])"/>に同じ.
+        /// </remarks>
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <param name="fourCornersSpan">取得した角の座標を格納する<see cref="Span{T}"/>.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void GetWorldCorners(this RectTransform target, in Span<Vector3> fourCornersSpan)
+        {
+            if (fourCornersSpan.IsEmpty || fourCornersSpan.Length < 4)
+            {
+                Debug.LogError("Calling GetWorldCorners with an array that is null or has less than 4 elements.");
+            }
+            else
+            {
+                target.GetCalculateLocalCorners(fourCornersSpan);
+                Matrix4x4 localToWorldMatrix = target.localToWorldMatrix;
+                for (int i = 0; i < fourCornersSpan.Length; i++)
+                {
+                    fourCornersSpan[i] = localToWorldMatrix.MultiplyPoint(fourCornersSpan[i]);
+                }
+            }
+        }
+
+        /// <summary>
+        /// <see cref="RectTransform"/>のローカル空間で計算された長方形の角を取得する.
+        /// <remarks>
+        /// <see cref="RectTransform.GetLocalCorners(Vector3[])"/>に同じ.
+        /// </remarks>
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <param name="fourCornersSpan">取得した角の座標を格納する<see cref="Span{T}"/>.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void GetLocalCorners(this RectTransform target, in Span<Vector3> fourCornersSpan)
+        {
+            if (fourCornersSpan.IsEmpty || fourCornersSpan.Length < 4)
+            {
+                Debug.LogError("Calling GetLocalCorners with an array that is null or has less than 4 elements.");
+            }
+            else
+            {
+                target.GetCalculateLocalCorners(fourCornersSpan);
+            }
+        }
+
+        /// <summary>
+        /// <see cref="RectTransform"/>のローカル空間で計算された長方形の角を取得する.
+        /// <remarks>
+        /// <see cref="RectTransform.GetLocalCorners(Vector3[])"/>に同じ.
+        /// </remarks>
+        /// </summary>
+        /// <param name="target">対象の<see cref="RectTransform"/>.</param>
+        /// <param name="fourCornersSpan">取得した角の座標を格納する<see cref="Span{T}"/>.</param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        internal static void GetCalculateLocalCorners(this RectTransform target, in Span<Vector3> fourCornersSpan)
+        {
+            Rect rect = target.rect;
+            (float x, float y, float xMax, float yMax) = (rect.x, rect.y, rect.xMax, rect.yMax);
+            fourCornersSpan[0] = new Vector3(x, y);
+            fourCornersSpan[1] = new Vector3(x, yMax);
+            fourCornersSpan[2] = new Vector3(xMax, yMax);
+            fourCornersSpan[3] = new Vector3(xMax, y);
         }
     }
 }
