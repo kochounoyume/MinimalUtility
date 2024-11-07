77c1e6c68c241d962ac38022d8901b0677737d74
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2024-11-07 09:50:15 +0900

    Fix MemoryExtensions

--- Packages/MinimalUtility/Runtime/MemoryExtensions.cs
+++ Packages/MinimalUtility/Runtime/MemoryExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 
 namespace MinimalUtility
@@ -15,7 +16,7 @@ namespace MinimalUtility
         /// <typeparam name="T">要素の型.</typeparam>
         /// <returns>要素を列挙する<see cref="Span{T}.Enumerator"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static Span<T>.Enumerator GetEnumerator<T>(in this Memory<T> memory) => memory.Span.GetEnumerator();
+        public static Enumerator<T> GetEnumerator<T>(in this Memory<T> memory) => new (memory);
 
         /// <summary>
         /// <see cref="ReadOnlyMemory{T}"/>のforeach対応.
@@ -24,6 +25,43 @@ namespace MinimalUtility
         /// <typeparam name="T">要素の型.</typeparam>
         /// <returns>要素を列挙する<see cref="ReadOnlySpan{T}.Enumerator"/>.</returns>
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static ReadOnlySpan<T>.Enumerator GetEnumerator<T>(in this ReadOnlyMemory<T> memory) => memory.Span.GetEnumerator();
+        public static Enumerator<T> GetEnumerator<T>(in this ReadOnlyMemory<T> memory) => new (memory);
+
+        /// <summary>
+        /// <see cref="ReadOnlyMemory{T}"/>のforeach対応.
+        /// </summary>
+        /// <typeparam name="T">要素の型.</typeparam>
+        public struct Enumerator<T> : IEnumerator<T>
+        {
+            private readonly ReadOnlyMemory<T> memory;
+            private int index;
+
+            /// <inheritdoc />
+            public T Current => memory.Span[index];
+
+            /// <inheritdoc />
+            object System.Collections.IEnumerator.Current => Current;
+
+            /// <summary>
+            /// Initializes a new instance of the <see cref="Enumerator{T}"/> struct.
+            /// </summary>
+            /// <param name="memory"><see cref="ReadOnlyMemory{T}"/>.</param>
+            internal Enumerator(in ReadOnlyMemory<T> memory)
+            {
+                this.memory = memory;
+                index = -1;
+            }
+
+            /// <inheritdoc />
+            public bool MoveNext() => index < memory.Length && ++index < memory.Length;
+
+            /// <inheritdoc />
+            public void Reset() => index = -1;
+
+            /// <inheritdoc />
+            void IDisposable.Dispose()
+            {
+            }
+        }
     }
 }
