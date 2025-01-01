ceb776064d8dcd324f23f05b48aad30fb928e771
Kochoyume <78918084+kochounoyume@users.noreply.github.com>
2025-01-01 22:50:59 +0900

    Add 'AddX()' and 'RemoveX()'  in TransformExtensions

--- Packages/MinimalUtility/Runtime/UnityExtension/TransformExtensions.cs
+++ Packages/MinimalUtility/Runtime/UnityExtension/TransformExtensions.cs
@@ -24,6 +24,32 @@ namespace MinimalUtility
         }
 
         /// <summary>
+        /// <see cref="Transform.position"/>のX成分を加算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="x"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddX(this Transform transform, in float x)
+        {
+            var position = transform.position;
+            position.x += x;
+            transform.position = position;
+        }
+
+        /// <summary>
+        /// <see cref="Transform.position"/>のX成分を減算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="x"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void RemoveX(this Transform transform, in float x)
+        {
+            var position = transform.position;
+            position.x -= x;
+            transform.position = position;
+        }
+
+        /// <summary>
         /// <see cref="Transform.position"/>のY成分を設定する.
         /// </summary>
         /// <param name="transform"></param>
@@ -37,6 +63,32 @@ namespace MinimalUtility
         }
 
         /// <summary>
+        /// <see cref="Transform.position"/>のY成分を加算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="y"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddY(this Transform transform, in float y)
+        {
+            var position = transform.position;
+            position.y += y;
+            transform.position = position;
+        }
+
+        /// <summary>
+        /// <see cref="Transform.position"/>のY成分を減算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="y"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void RemoveY(this Transform transform, in float y)
+        {
+            var position = transform.position;
+            position.y -= y;
+            transform.position = position;
+        }
+
+        /// <summary>
         /// <see cref="Transform.position"/>のZ成分を設定する.
         /// </summary>
         /// <param name="transform"></param>
@@ -46,6 +98,32 @@ namespace MinimalUtility
         {
             var position = transform.position;
             position.z = z;
+            transform.position = position;
+        }
+
+        /// <summary>
+        /// <see cref="Transform.position"/>のZ成分を加算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="z"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddZ(this Transform transform, in float z)
+        {
+            var position = transform.position;
+            position.z += z;
+            transform.position = position;
+        }
+
+        /// <summary>
+        /// <see cref="Transform.position"/>のZ成分を減算する.
+        /// </summary>
+        /// <param name="transform"></param>
+        /// <param name="z"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void RemoveZ(this Transform transform, in float z)
+        {
+            var position = transform.position;
+            position.z -= z;
             transform.position = position;
         }
     }
