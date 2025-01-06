04bb94c6175bf731bc5f36e1a1c5348eed377df8
Masatake Iwasaki <78918084+kochounoyume@users.noreply.github.com>
2025-01-06 14:28:18 +0900

    Update AddTo in DisposableExtensions

--- Packages/MinimalUtility/Runtime/DisposableExtensions.cs
+++ Packages/MinimalUtility/Runtime/DisposableExtensions.cs
@@ -21,5 +21,17 @@ namespace MinimalUtility
         {
             disposableContainer = disposable;
         }
+
+        /// <summary>
+        /// <see cref="IDisposable"/>を指定した<see cref="IDisposable"/>コンテナに追加します.
+        /// </summary>
+        /// <param name="disposable">任意の<see cref="IDisposable"/>実装構造体.</param>
+        /// <param name="disposableContainer">追加先の<see cref="IDisposable"/>コンテナ.</param>
+        /// <typeparam name="T">追加した<see cref="IDisposable"/>.</typeparam>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AddTo<T>(this T disposable, ref T disposableContainer) where T : struct, IDisposable
+        {
+            disposableContainer = disposable;
+        }
     }
 }
